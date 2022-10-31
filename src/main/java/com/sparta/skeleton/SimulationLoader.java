7ec9a96873e76b5e0b4b3a7337a609a9b613d1ad
Abdulhadi Al-Sayed <ahayed@yahoo.com>
2022-10-31 16:19:59 +0000

    Added log package for logging

--- src/main/java/com/sparta/skeleton/SimulationLoader.java
+++ src/main/java/com/sparta/skeleton/SimulationLoader.java
@@ -1,4 +1,17 @@
 package com.sparta.skeleton;
 
+import com.sparta.skeleton.util.log.LoggerSingleton;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class SimulationLoader {
+
+    Logger logger = LoggerSingleton.getSingleton().getLogger();
+
+    public void start() {
+        logger.log(Level.INFO, "Example");
+
+    }
+
 }
---
+++ src/main/java/com/sparta/skeleton/util/log/CustomLoggerConfiguration.java
@@ -0,0 +1,14 @@
+package com.sparta.skeleton.util.log;
+
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class CustomLoggerConfiguration {
+	public static void configureLogger(Logger logger, Level selectedLevel) {
+		logger.setUseParentHandlers(false); //Don't use any logging from the root logger
+		logger.addHandler(CustomFileHandler.getFileHandler());
+		logger.addHandler(CustomConsoleHandler.getConsoleHandler());
+		logger.setLevel(selectedLevel);
+	}
+}
