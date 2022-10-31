a682e2bd145cd338bd4f5e587c94df99e0a40ab3
HamzMas <32570820+HamzMas@users.noreply.github.com>
2022-10-31 17:26:10 +0000

    Update Trainee and TrainingCentre classes

--- src/main/java/com/sparta/skeleton/model/TrainingCentre.java
+++ src/main/java/com/sparta/skeleton/model/TrainingCentre.java
@@ -1,4 +1,40 @@
 package com.sparta.skeleton.model;
 
+import java.util.ArrayList;
+
 public class TrainingCentre {
+
+    int trainingCentreID = 1;
+    static int increment = 1;
+    private final int maxCapacity = 100;
+    private ArrayList<Trainee> traineeList = new ArrayList<>();
+
+    public TrainingCentre() {
+        trainingCentreID = increment;
+        increment++;
+    }
+
+    public int getCurrentCapacity() {
+        return traineeList.size();
+    }
+
+    public boolean trainingCentreIsFull() {
+        return traineeList.size() == maxCapacity;
+    }
+
+    public void addTrainee(Trainee trainee) {
+        if(traineeList.size() < maxCapacity) {
+            traineeList.add(trainee);
+        }
+    }
+
+    public int getTrainingCentreID() {
+        return trainingCentreID;
+    }
+
+    public ArrayList<Trainee> getTraineeList() {
+        return traineeList;
+    }
+
+
 }
