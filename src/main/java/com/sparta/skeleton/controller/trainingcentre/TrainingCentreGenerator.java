ec95fb230aa8fee8d414a4decf22e06f0617b1cb
FranciscoCavaco <fg_cavaco@hotmail.com>
2022-10-31 17:09:07 +0000

    Added comments for Training Manager

--- src/main/java/com/sparta/skeleton/controller/trainingcentre/TrainingCentreGenerator.java
+++ src/main/java/com/sparta/skeleton/controller/trainingcentre/TrainingCentreGenerator.java
@@ -1,4 +1,20 @@
 package com.sparta.skeleton.controller.trainingcentre;
 
+import com.sparta.skeleton.model.TrainingCentre;
+
 public class TrainingCentreGenerator {
+
+
+    //Call Training Centre object and create new
+    //To String method
+
+
+
+    //USER STORIES
+    //Generation for training Centre
+
+    public generateTrainingCentre() {
+        TrainingCentre trainingCentre = new TrainingCentre();
+    }
+    //
 }
