47755ef5f74a5bce3b2ebbfe26cc871fda32cbf6
riya353 <riyashaji353@gmail.com>
2022-10-31 17:54:07 +0000

    added interfaces for methods that need to be implemented

---
+++ src/main/java/com/sparta/skeleton/controller/trainingcentre/TrainingCentreGeneratable.java
@@ -0,0 +1,11 @@
+package com.sparta.skeleton.controller.trainingcentre;
+
+import com.sparta.skeleton.model.TrainingCentre;
+
+public interface TrainingCentreGeneratable {
+
+    //generates training centre
+    TrainingCentre generate();
+
+
+}
