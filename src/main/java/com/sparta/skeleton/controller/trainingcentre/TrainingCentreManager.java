b0d528f226eac4dc7603d3d945dfb984fff500ac
FranciscoCavaco <fg_cavaco@hotmail.com>
2022-11-01 14:34:24 +0000

    Training Centre generator and manager methods implemented

--- src/main/java/com/sparta/skeleton/controller/trainingcentre/TrainingCentreManager.java
+++ src/main/java/com/sparta/skeleton/controller/trainingcentre/TrainingCentreManager.java
@@ -3,46 +3,44 @@ package com.sparta.skeleton.controller.trainingcentre;
 import com.sparta.skeleton.model.Trainee;
 import com.sparta.skeleton.model.TrainingCentre;
 
+import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.Queue;
+import java.util.Random;
 
-public class TrainingCentreManager implements TrainingCentreManageable{
+public class TrainingCentreManager {
 
 
-    @Override
-    public int getTraineeCount(ArrayList<TrainingCentre> trainingCentre) {
+
+    public static int getTraineeCount(ArrayList<TrainingCentre> trainingCentre) {
         return trainingCentre.size();
     }
 
-    @Override
-    public int populateTrainingCentre(Queue<Trainee> traineeList, TrainingCentre trainingCentre, int uptake) {
-        int resultingUptake;
 
-        for (int i =0; i<uptake; i++){
-            resultingUptake = uptake;
-            if (traineeList.size() < 0) {
+    public static int populateTrainingCentre(Queue<Trainee> traineeQueue, TrainingCentre trainingCentre, int uptake) {
+        int resultingUptake = uptake;
+        int loopMax = Math.min(uptake, trainingCentre.getCurrentCapacity());
+
+        for (int i =0; i<loopMax; i++){
+            if (traineeQueue.size() < 1) {
                 return resultingUptake;
             } else {
-                traineeList.remove();
+                trainingCentre.addTrainee(traineeQueue.remove());
                 resultingUptake =- 1;
-
             }
         }
-
-        return 0;
+        return resultingUptake;
     }
 
-    @Override
-    public int generateRandomTraineeUptake() {
-        return 0;
+    public static int generateRandomTraineeUptake() {
+        Random random = new Random();
+
+        return random.nextInt(51);
     }
 
     //USER STORIES
     //trainees populate training centre
     //Full capacity Management
     //Prioritize waiting list (? ask if its in here)
-
-
-
 
 }
