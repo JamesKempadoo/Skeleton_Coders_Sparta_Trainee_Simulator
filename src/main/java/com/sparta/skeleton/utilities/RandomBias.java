d7c5e4f83b83290d0413e5ccace4883d587b3ac8
FranciscoCavaco <fg_cavaco@hotmail.com>
2022-11-03 11:41:15 +0000

    added Test for summary of Million random Numbers generated

--- src/main/java/com/sparta/skeleton/utilities/RandomBias.java
+++ src/main/java/com/sparta/skeleton/utilities/RandomBias.java
@@ -11,15 +11,15 @@ public class RandomBias {
 
         System.out.println("Random double generated: " + num);
 
-        if (num < 0.8){
+        if (num < 0.8){ // 80% chance
             return randomInt.nextInt(15, 50);
-        } else if (num < 0.9) {
+        } else if (num < 0.9) { // 10% chance
             return randomInt.nextInt(50, 100);
-        } else if (num < 0.97) {
+        } else if (num < 0.97) { // 7% chance
             return  randomInt.nextInt(100, 300);
-        } else if (num < 0.995) {
+        } else if (num < 0.995) { // 2.5% chance
             return  randomInt.nextInt(300, 1000);
-        } else {
+        } else { // 0.5% chance
             return Math.abs(randomInt.nextInt());
         }
     }
--- src/test/java/com/sparta/skeleton/RandomBiasTest.java
+++ src/test/java/com/sparta/skeleton/RandomBiasTest.java
@@ -6,9 +6,40 @@ import org.junit.jupiter.api.Test;
 public class RandomBiasTest {
 
     @Test
-    void checkRandom(){
+    void display100RandomNumbers(){
 
         for (int i = 0; i<101 ; i++)
             System.out.println(RandomBias.randomBiasGenerator());
+    }
+
+    @Test
+    void summaryOf1000000RandomNumbers(){
+        int between15And50 = 0;
+        int between50And100 = 0;
+        int between100And300 = 0;
+        int between300And1000 = 0;
+        int over1000 = 0;
+        final double MILL = 1000000;
+
+        for (int i = 0; i<1000001 ; i++){
+            int tempInt = RandomBias.randomBiasGenerator();
+            if (tempInt < 50)  {
+                between15And50++;
+            } else if (tempInt < 100) {
+                between50And100++;
+            } else if (tempInt < 300){
+                between100And300++;
+            } else if (tempInt < 1000) {
+                between300And1000++;
+            } else {
+                over1000++;
+            }
+        }
+
+        System.out.println("Between 15 and 50: " + between15And50 + " -> " + (between15And50/MILL)*100 + "%");
+        System.out.println("Between 50 and 100: " + between50And100 + " -> " + (between50And100/MILL)*100 + "%");
+        System.out.println("Between 100 and 300: " + between100And300 + " -> " + (between100And300/MILL)*100 + "%");
+        System.out.println("Between 300 and 1000: " + between300And1000 + " -> " + (between300And1000/MILL)*100 + "%");
+        System.out.println("Over 1000: " + over1000 + " -> " + (over1000/MILL)*100 + "%");
     }
 }
