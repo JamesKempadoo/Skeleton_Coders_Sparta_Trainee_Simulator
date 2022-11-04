49dcc92e12c439a291a25404fa5742574e330380
HamzMas <32570820+HamzMas@users.noreply.github.com>
2022-11-04 11:50:50 +0000

    test

--- src/test/java/com/sparta/skeleton/TraineeTest.java
+++ src/test/java/com/sparta/skeleton/TraineeTest.java
@@ -13,24 +13,11 @@ public class TraineeTest {
     private static Trainee trainee2 = new Trainee();
 
     @Test
-    @DisplayName("Test that the first Trainee created has a traineeID of 1")
-    void testFirstTraineeCreated() {
-        Assertions.assertEquals(1,trainee.getTraineeID());
-    }
-
-
-    @Test
     @DisplayName("Test that a trainee is created with a correct course type")
     void testThatATraineeIsCreatedWithACorrectCourseType() {
         String[] courses = {"Java", "C#", "Data", "DevOps", "Business"};
         String course = trainee.getCourseType();
         Assertions.assertTrue(Arrays.asList(courses).contains(course));
-    }
-
-    @Test
-    @DisplayName("Test that the second Trainee created has a traineeID of 2")
-    void testSecondTraineeCreated() {
-        Assertions.assertEquals(2, trainee2.getTraineeID());
     }
 
     @Test
@@ -41,7 +28,5 @@ public class TraineeTest {
         }
         Assertions.assertEquals(5, trainee.getMonthsTrained());
     }
-
-
 }
 
